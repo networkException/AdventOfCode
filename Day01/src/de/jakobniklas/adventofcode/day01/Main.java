baac70788932435d9e9755f13f27af6c45881b42
Jakob-Niklas See <jakobniklas@icloud.com>
2019-12-02 21:14:45 +0100

    Setup workspace

---
+++ Day01/src/de/jakobniklas/adventofcode/day01/Main.java
@@ -0,0 +1,9 @@
+package de.jakobniklas.adventofcode.day01;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ Day01/src/de/jakobniklas/adventofcode/day01/MassCalculator.java
@@ -0,0 +1,6 @@
+package de.jakobniklas.adventofcode.day01;
+
+public class MassCalculator
+{
+
+}
