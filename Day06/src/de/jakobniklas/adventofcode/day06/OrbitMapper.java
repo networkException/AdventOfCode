de02bb9939904ffc425170f90620545bf397a3ad
Jakob-Niklas See <jakobniklas@icloud.com>
2019-12-06 18:46:31 +0100

    Hopefully fixed error in findTraceIntersection
    waiting for submission timeout skateSAD

--- Day06/src/de/jakobniklas/adventofcode/day06/OrbitMapper.java
+++ Day06/src/de/jakobniklas/adventofcode/day06/OrbitMapper.java
@@ -2,11 +2,11 @@ package de.jakobniklas.adventofcode.day06;
 
 import de.jakobniklas.adventofcode.day06.orbit.SpaceObject;
 import de.jakobniklas.applicationlib.commonutil.FileUtil;
-import de.jakobniklas.applicationlib.commonutil.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class OrbitMapper
@@ -33,7 +33,23 @@ public class OrbitMapper
 
             while(!parent.getSpaceObject().equals("COM"))
             {
-                spaceObject.getTrace().add(parent);
+                AtomicBoolean filledTrace = new AtomicBoolean(false);
+
+                //Prevent double trace fill
+                spaceObject.getTrace().forEach((traceElement) ->
+                {
+                    if(traceElement.getParent().equals("COM"))
+                    {
+                        filledTrace.set(true);
+                    }
+                });
+
+                if(!filledTrace.get())
+                {
+                    spaceObject.getTrace().add(parent);
+                }
+
+                //Recursion
                 parent = getParent(parent);
                 count.getAndIncrement();
             }
@@ -50,7 +66,7 @@ public class OrbitMapper
             int transfersFromYou = transfersToObject(intersection, you);
             int transfersFromSanta = transfersToObject(intersection, santa);
 
-            count.set(transfersFromYou + transfersFromSanta);
+            count.set(transfersFromYou + transfersFromSanta - 2);
         }
 
         return count.get();
@@ -58,37 +74,23 @@ public class OrbitMapper
 
     public int transfersToObject(SpaceObject to, SpaceObject from)
     {
-        AtomicInteger count = new AtomicInteger(0);
-
-        from.getTrace().forEach((traceElement) ->
-        {
-            if(traceElement.getSpaceObject().equals(to.getSpaceObject()))
-            {
-                count.getAndIncrement();
-
-                Log.print(from.getSpaceObject(), to.getSpaceObject());
-            }
-        });
-
-        return count.get();
+        return from.getTrace().indexOf(to);
     }
 
     public SpaceObject findTraceIntersection(SpaceObject first, SpaceObject second)
     {
         AtomicInteger highest = new AtomicInteger(0);
 
-        first.getTrace().forEach((traceElement) ->
+        for(SpaceObject traceElement : first.getTrace())
         {
             int indexOf = second.getTrace().indexOf(traceElement);
 
             if(indexOf != -1)
             {
-                if(indexOf < highest.get())
-                {
-                    highest.set(indexOf);
-                }
+                highest.set(indexOf);
+                break;
             }
-        });
+        }
 
         return second.getTrace().get(highest.get() + 1);
     }
