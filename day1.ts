9abde028b42ab8c10781b4a9824a048a4bda195b
networkException <git@nwex.de>
2020-12-01 13:06:17 +0100

    Day1: Improved code at least a little bit

--- day1.ts
+++ day1.ts
@@ -1,12 +1,12 @@
 import * as files from 'fs';
 
-const input: string = files.readFileSync('input/day1.txt').toString();
+const input: Array<number> = files.readFileSync('input/day1.txt').toString().split('\n').map(Number);
 
 (() => {
-    for (const line of input.split('\n')) {
-        for (const line2 of input.split('\n')) {
-            if (Number(line) + Number(line2) === 2020) {
-                console.log('Day 1A: The output is ' + Number(line) * Number(line2));
+    for (const line of input) {
+        for (const line2 of input) {
+            if (line + line2 === 2020) {
+                console.log('Day 1A: The output is ' + line * line2);
                 return;
             }
         }
@@ -14,11 +14,11 @@ const input: string = files.readFileSync('input/day1.txt').toString();
 })();
 
 (() => {
-    for (const line of input.split('\n')) {
-        for (const line2 of input.split('\n')) {
-            for (const line3 of input.split('\n')) {
-                if (Number(line) + Number(line2) + Number(line3) === 2020) {
-                    console.log('Day 1B: The output is ' + Number(line) * Number(line2) * Number(line3));
+    for (const line of input) {
+        for (const line2 of input) {
+            for (const line3 of input) {
+                if (line + line2 + line3 === 2020) {
+                    console.log('Day 1B: The output is ' + (line * line2 * line3));
                     return;
                 }
             }
