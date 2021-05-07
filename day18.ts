836578940bfdcb4a6e305efc3e84ccd3e65d6ff6
networkException <git@nwex.de>
2021-05-07 15:54:01 +0200

    README+Day18: Solve day 18 part b

--- day18.ts
+++ day18.ts
@@ -1,6 +1,6 @@
 import * as files from 'fs';
 
-type TokenType = 'literal' | 'plus' | 'asterisk' | 'parenOpen' | 'parenClose';
+type TokenType = 'literal' | 'plus' | 'asterisk' | 'parenOpen' | 'parenClose' | 'end';
 
 interface Token {
     type: TokenType;
@@ -25,6 +25,10 @@ class LiteralExpression extends Expression {
     public evaluate(): number {
         return this.value;
     }
+
+    public toString(): string {
+        return this.value.toString();
+    }
 }
 
 type BinaryOperation = 'add' | 'multiply';
@@ -44,11 +48,15 @@ class BinaryExpression extends Expression {
 
         console.error('BinaryExpression: Unimplemented operation ' + this.operation);
     }
+
+    public toString(): string {
+        return `${this.lhs} ${this.operation} ${this.rhs}`;
+    }
 }
 
 const input: Array<string> = files.readFileSync('input/day18.txt').toString().split('\n');
 
-const compute = (source: string): number => {
+const compute = (source: string, usePrecedence: boolean = false): number => {
     let tokens: Array<Token> = new Array<Token>();
 
     const matches = (value: string): boolean => source.startsWith(value);
@@ -80,12 +88,12 @@ const compute = (source: string): number => {
     }
 
     const current = (offset: number = 0): Token => tokens[offset];
-    const consume = (type: TokenType): Token => {
+    const consume = (type?: TokenType): Token => {
         const consumed: Token = tokens[0];
 
         tokens = tokens.slice(1);
 
-        if (consumed.type !== type) {
+        if (type && consumed.type !== type) {
             console.error(`Consume: Expected token of type ${type} but got ${consumed.type} instead`);
 
             process.exit(1);
@@ -94,38 +102,29 @@ const compute = (source: string): number => {
         return consumed;
     };
 
-    const matchSecondaryExpression = (): boolean => {
-        if (!current())
-            return false;
-
-        switch (current().type) {
+    const parseBinaryOperation = (token: Token): BinaryOperation => {
+        switch (token.type) {
             case 'plus':
+                return 'add';
             case 'asterisk':
-                return true;
-            default:
-                return false;
+                return 'multiply';
         }
-    };
 
-    const parseExpression = (): Expression => {
-        let expression: Expression = parsePrimaryExpression();
+        console.log('BinaryOperation: Unable to parse ' + token.type);
 
-        while (matchSecondaryExpression())
-            expression = parseSecondaryExpression(expression);
-
-        return expression;
+        process.exit(1);
     };
 
     const parsePrimaryExpression = (): Expression => {
-        if (current().type === 'literal')
-            return new LiteralExpression(consume('literal').value);
+        switch (current().type) {
+            case 'literal':
+                return new LiteralExpression(consume('literal').value);
+            case 'parenOpen':
+                consume('parenOpen');
+                const expression: Expression = parseExpression();
+                consume('parenClose');
 
-        if (current().type === 'parenOpen') {
-            consume('parenOpen');
-            const expression: Expression = parseExpression();
-            consume('parenClose');
-
-            return expression;
+                return expression;
         }
 
         console.log('PrimaryExpression: Unable to parse ' + current().type);
@@ -133,27 +132,83 @@ const compute = (source: string): number => {
         process.exit(1);
     };
 
-    const parseSecondaryExpression = (primary: Expression): Expression => {
-        switch (current().type) {
-            case 'plus':
-                consume('plus');
+    let parseExpression: () => Expression;
+    let parseSecondaryExpression: (lhs: Expression, minPrecendence?: number) => Expression;
 
-                // If rhs of this is parseExpression(), it will recursively parse all secondary expressions.
-                // This creates a tree, that is not desired in this case.
-                // Here we want to read the whole expression from left to right, meaning that the inner most
-                // expression in the tree is either the one all the way to the left or one deeply nested in parens.
+    if (usePrecedence) {
+        emit('end', '');
 
-                return new BinaryExpression(primary, 'add', parsePrimaryExpression());
-            case 'asterisk':
-                consume('asterisk');
+        const precedence: { [type in TokenType]?: number } = {
+            'plus': 2,
+            'asterisk': 1
+        };
 
-                return new BinaryExpression(primary, 'multiply', parsePrimaryExpression());
-        }
+        parseExpression = () => {
+            return parseSecondaryExpression(parsePrimaryExpression(), 0);
+        };
 
-        console.log('SecondaryExpression: Unable to parse ' + current().type);
+        // See https://en.wikipedia.org/wiki/Operator-precedence_parser#Pseudocode
+        parseSecondaryExpression = (lhs: Expression, minPrecendence?: number): Expression => {
+            if (minPrecendence === undefined) {
+                console.log('SecondaryExpression: This mode requires minPrecendence');
 
-        process.exit(1);
-    };
+                process.exit(1);
+            }
+
+            let lookahead: Token = current();
+
+            while (lookahead.type in precedence && precedence[lookahead.type] >= minPrecendence) {
+                const operation: Token = lookahead;
+
+                consume(operation.type);
+
+                let rhs: Expression = parsePrimaryExpression();
+
+                lookahead = current();
+
+                while (lookahead.type in precedence && precedence[lookahead.type] > precedence[operation.type]) {
+                    rhs = parseSecondaryExpression(rhs, minPrecendence + 1);
+
+                    lookahead = current();
+                }
+
+                lhs = new BinaryExpression(lhs, parseBinaryOperation(operation), rhs);
+            }
+
+            return lhs;
+        };
+    } else {
+        const matchSecondaryExpression = (): boolean => {
+            if (!current())
+                return false;
+
+            switch (current().type) {
+                case 'plus':
+                case 'asterisk':
+                    return true;
+                default:
+                    return false;
+            }
+        };
+
+        parseExpression = (): Expression => {
+            let expression: Expression = parsePrimaryExpression();
+
+            while (matchSecondaryExpression())
+                expression = parseSecondaryExpression(expression);
+
+            return expression;
+        };
+
+        parseSecondaryExpression = (primary: Expression): Expression => {
+            // If rhs of this is parseExpression(), it will recursively parse all secondary expressions.
+            // This creates a tree, that is not desired in this case.
+            // Here we want to read the whole expression from left to right, meaning that the inner most
+            // expression in the tree is either the one all the way to the left or one deeply nested in parens.
+
+            return new BinaryExpression(primary, parseBinaryOperation(consume()), parsePrimaryExpression());
+        };
+    }
 
     const expression: Expression = parseExpression();
 
@@ -164,3 +219,4 @@ const compute = (source: string): number => {
 };
 
 console.log(`Day 18A: The sum of the homework expressions is ${input.map(source => compute(source)).reduce((a, b) => a + b)}`);
+console.log(`Day 18B: The sum of the homework expressions with operator precedence is ${input.map(source => compute(source, true)).reduce((a, b) => a + b)}`);
