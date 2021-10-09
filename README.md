42f2ed5ff049fd22448ffa736017c9983735f82b
networkException <git@nwex.de>
2021-10-09 12:17:02 +0200

    Everywhere: Prepare project root for 2021

---
+++ README.md
@@ -0,0 +1,25 @@
+# AdventOfCode
+
+networkException's Advent of Code solutions
+
+## 2021
+
+[![About](https://img.shields.io/badge/Advent%20of%20Code-About-red)](https://adventofcode.com/2020/about)
+[![Days completed](https://img.shields.io/badge/Days%20completed-0-brightgreen)](https://github.com/networkException/AdventOfCode/)
+[![Stars](https://img.shields.io/badge/Stars-0-yellow)](https://github.com/networkException/AdventOfCode/)
+[![Language](https://img.shields.io/badge/Language-TypeScript-blue)](https://www.typescriptlang.org/)
+[![Environment](https://img.shields.io/badge/Environment-Node-brightgreen)](https://nodejs.org/en/)
+[![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD--3--Clause-red.svg)](https://spdx.org/licenses/BSD-3-Clause.html)
+
+### Run instructions
+
+Use `npm start [ days... | all ]` for running one or more days.
+Use `npm test` for linting.
+
+### Check out other solutions
+
++ Nothing here yet
+
+## Previous years
+
+Previous solutions can be found in the `previous` directory.
---
+++ harness.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021, Jakob-Niklas See <git@nwex.de>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import { exec, execSync } from 'child_process';
+import { readdirSync } from 'fs';
+
+const days = process.argv.slice(2);
+
+if (days.length < 1) {
+    console.error('Please specify a valid day to run');
+    process.exit(1);
+}
+
+if (days[0] === 'all') {
+    execSync('tsc -p tsconfig.json', { encoding: 'utf8' });
+
+    (async () => {
+        console.time('all');
+
+        for (const file of readdirSync('.').filter(name => name.startsWith('day') && name.endsWith('.js'))) {
+            await new Promise<void>(resolve => exec(`node ${file}`, (error, out) => {
+                console.log(out);
+                resolve();
+            }));
+        }
+
+        console.timeLog('all', 'Execution of all days (with overhead of this being run in node)');
+    })();
+
+    process.exit(0);
+}
+
+if (days.length === 1) {
+    execSync(`ts-node ${days[0]}.ts`, { encoding: 'utf8' });
+
+    process.exit(0);
+}
+
+(async () => {
+    execSync('tsc -p tsconfig.json', { encoding: 'utf8' });
+
+    console.time('all');
+
+    for (const day of days) {
+        await new Promise<void>(resolve => exec(`node ${day}.js`, (error, out) => {
+            console.log(out);
+            resolve();
+        }));
+    }
+
+    // @ts-expect-error Intl is a stage 3 proposal and as such has no typings
+    const list = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+
+    console.timeLog('all', `Execution of days ${list.format(days)} (with overhead of this being run in node)`);
+
+    process.exit(0);
+})();
