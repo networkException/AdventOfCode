97921c5153ac9eb6eacba9628bd16bf3605380a9
networkException <git@nwex.de>
2021-12-02 16:09:43 +0100

    Day02: Initial commit and solution

---
+++ day2.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from 'fs';
+
+const instructions = readFileSync('input/day2').toString().split('\n');
+
+{
+    const position = {
+        horizontal: 0,
+        depth: 0
+    };
+
+    for (const instruction of instructions) {
+        const action = instruction.split(' ')[0];
+        const value = Number(instruction.split(' ')[1]);
+
+        switch (action) {
+            case 'forward': position.horizontal += value; break;
+            case 'down': position.depth += value; break;
+            case 'up': position.depth -= value; break;
+        }
+    }
+
+    console.log(`day2/a: You get ${position.horizontal * position.depth} if you multiply your final horizontal position by your final depth`);
+}
+
+{
+    const position = {
+        aim: 0,
+        horizontal: 0,
+        depth: 0
+    };
+
+    for (const instruction of instructions) {
+        const action = instruction.split(' ')[0];
+        const value = Number(instruction.split(' ')[1]);
+
+        switch (action) {
+            case 'forward': position.horizontal += value; position.depth += position.aim * value; break;
+            case 'down': position.aim += value; break;
+            case 'up': position.aim -= value; break;
+        }
+    }
+
+    console.log(`day2/b: You get ${position.horizontal * position.depth} if you multiply your final horizontal position by your final depth`);
+}
