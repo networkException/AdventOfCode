adee5578b46390e9b35a58f0a6cf4dd39a498acd
networkException <git@nwex.de>
2021-12-08 17:15:49 +0100

    Day08: Simplify lookup logic

--- day8.ts
+++ day8.ts
@@ -41,11 +41,7 @@ const entries = readFileSync('input/day8').toString().split('\n').map(entry => e
 
     const findWithPatternLength = (patterns: Array<string>, length: number): string | undefined => patterns.find(pattern => pattern.length === length);
     const numberOfOtherPatternsWith = (patterns: Array<string>, character: string): number => patterns.filter(pattern => pattern.includes(character)).length;
-    const unknownCharactersInPattern = (known: Record<string, string>, pattern: string): Array<string> => {
-        const knownValues = Object.values(known);
-
-        return pattern.split('').filter(character => !knownValues.includes(character));
-    };
+    const unknownCharactersInPattern = (known: Record<string, string>, pattern: string): Array<string> => pattern.split('').filter(character => known[character] === undefined);
 
     let result = 0;
 
@@ -56,42 +52,40 @@ const entries = readFileSync('input/day8').toString().split('\n').map(entry => e
 
         const one = findWithPatternLength(patterns, 2) as string; // A one should exactly exist once in the input pattern list, ignoring undefined
         if (numberOfOtherPatternsWith(patterns, one[0]) === 9) { // All digits except "2" have character f, which is either the first or the second character in "one"
-            known['f'] = one[0];
-            known['c'] = one[1];
+            known[one[0]] = 'f';
+            known[one[1]] = 'c';
         } else {
-            known['f'] = one[1];
-            known['c'] = one[0];
+            known[one[1]] = 'f';
+            known[one[0]] = 'c';
         }
 
         const seven = findWithPatternLength(patterns, 3) as string;
-        known['a'] = unknownCharactersInPattern(known, seven)[0]; // Seven consists of all characters found in "one" and an additional one
+        known[unknownCharactersInPattern(known, seven)[0]] = 'a'; // Seven consists of all characters found in "one" and an additional one
 
         const four = findWithPatternLength(patterns, 4) as string;
         const unknownFourCharacters = unknownCharactersInPattern(known, four); // Four consists of all characters found in one as well as b and d, of which b only occurs 6 times in all digits
         if (numberOfOtherPatternsWith(patterns, unknownFourCharacters[0]) === 6) {
-            known['b'] = unknownFourCharacters[0];
-            known['d'] = unknownFourCharacters[1];
+            known[unknownFourCharacters[0]] = 'b';
+            known[unknownFourCharacters[1]] = 'd';
         } else {
-            known['b'] = unknownFourCharacters[1];
-            known['d'] = unknownFourCharacters[0];
+            known[unknownFourCharacters[1]] = 'b';
+            known[unknownFourCharacters[0]] = 'd';
         }
 
         const eight = findWithPatternLength(patterns, 7) as string;
         const unknownEightCharacters = unknownCharactersInPattern(known, eight); // Eight consists of all characters, only e and g havent been found yet and e only occurs 4 times in all digits
         if (numberOfOtherPatternsWith(patterns, unknownEightCharacters[0]) === 4) {
-            known['e'] = unknownEightCharacters[0];
-            known['g'] = unknownEightCharacters[1];
+            known[unknownEightCharacters[0]] = 'e';
+            known[unknownEightCharacters[1]] = 'g';
         } else {
-            known['e'] = unknownEightCharacters[1];
-            known['g'] = unknownEightCharacters[0];
+            known[unknownEightCharacters[1]] = 'e';
+            known[unknownEightCharacters[0]] = 'g';
         }
-
-        const lookupTable = Object.fromEntries(Object.entries(known).map(([ known, translated ]) => [ translated, known ]));
 
         let output = '';
 
         for (const outputPattern of outputPatterns) {
-            const decoded = outputPattern.split('').map(character => lookupTable[character]).sort();
+            const decoded = outputPattern.split('').map(character => known[character]).sort();
 
             for (let i = 0; i < digits.length; i++) {
                 if (decoded.join('') === digits[i].join('')) {
